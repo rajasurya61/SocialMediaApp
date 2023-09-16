c278360d12d376884d807e2957c56cb1e6d7ec04
rajas <rajas@SK_HP>
2023-09-16 18:17:18 -0500

    Add project files.

---
+++ SocialMediaApp/Models/SocialMediaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SocialMediaApp.Models
+{
+    public class SocialMediaDbContext : DbContext
+    {
+        public SocialMediaDbContext(DbContextOptions<SocialMediaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ SocialMediaApp/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        // Add more properties as needed
+    }
+}
