9a22876df9d68136bd6fb58c57842a48c5412180
Raja Surya <rajasurya4399@gmail.com>
2023-09-16 18:33:14 -0500

    Added Controller

---
+++ SocialMediaApp/SocialMediaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaApp.Models;
+
+namespace SocialMediaApp
+{
+    public class SocialMediaDbContext : DbContext
+    {
+        public SocialMediaDbContext(DbContextOptions<SocialMediaDbContext> options): base(options){}
+
+        public DbSet<User> Users { get; set; }
+    }
+}
